46ebd9374edfdedf7177d655b2d005a6b0208754
deepak bhatt <deepakbhattmits@gmail.com>
2020-08-22 13:02:24 +0530

    Update DrawerNavigator.js
    display favorites navigator in drawer screen.

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import TabNavigator from './TabNavigator';
+// import Favorites from '../screens/Favorites';
+import {MainStackNavigator, FavoritesStackNavigator} from './StackNavigator';
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
   return (
     <Drawer.Navigator initialRouteName="Home">
       <Drawer.Screen name="Home" component={TabNavigator} />
+      <Drawer.Screen name="Favorite" component={FavoritesStackNavigator} />
     </Drawer.Navigator>
   );
 };
